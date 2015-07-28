de0a8c204278e04b2f6b8ccb4d0754546c10cfd3
Dorothy Thurston <dorothy.thur@gmail.com>
2015-07-28 12:07:15 -0400

    Add brush settings actions and draw on image actions

--- PicPaint/BrushSettingsViewController.swift
+++ PicPaint/BrushSettingsViewController.swift
@@ -8,9 +8,34 @@
 
 import UIKit
 
+protocol BrushSettingsViewControllerDelegate {
+    func settingsViewControllerFinished(settingsViewController: BrushSettingsViewController)
+}
+
 class BrushSettingsViewController: UIViewController {
+    // MARK: - Properties
+    var brush: CGFloat = 10.0
+    var opacity: CGFloat = 1.0
+    var red: CGFloat = 0.0
+    var green: CGFloat = 0.0
+    var blue: CGFloat = 0.0
+    
+    @IBOutlet weak var sliderBrush: UISlider!
+    @IBOutlet weak var sliderOpacity: UISlider!
+    
+    @IBOutlet weak var imageViewBrush: UIImageView!
+    
+    @IBOutlet weak var sliderRed: UISlider!
+    @IBOutlet weak var sliderGreen: UISlider!
+    @IBOutlet weak var sliderBlue: UISlider!
+    
+    var delegate: BrushSettingsViewControllerDelegate?
+    
+    //MARK: - Actions
 
     @IBAction func didPressClose(sender: UIBarButtonItem) {
+        dismissViewControllerAnimated(true, completion: nil)
+        self.delegate?.settingsViewControllerFinished(self)
     }
     
     override func viewDidLoad() {
@@ -24,15 +49,37 @@ class BrushSettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func colorChanged(sender: UISlider) {
+        red = CGFloat(sliderRed.value / 255.0)
+        green = CGFloat(sliderGreen.value / 255.0)
+        blue = CGFloat(sliderBlue.value / 255.0)
+        
+        drawPreview()
     }
-    */
-
+    
+    @IBAction func propertyChanged(sender: UISlider) {
+        if sender == sliderBrush {
+            brush = CGFloat(sender.value)
+        } else {
+            opacity = CGFloat(sender.value)
+        }
+        
+        drawPreview()
+    }
+    
+    func drawPreview() {
+        UIGraphicsBeginImageContext(imageViewBrush.frame.size)
+        var context = UIGraphicsGetCurrentContext()
+        
+        CGContextSetLineCap(context, kCGLineCapRound)
+        CGContextSetLineWidth(context, brush)
+        CGContextMoveToPoint(context, 45.0, 45.0)
+        CGContextAddLineToPoint(context, 45.0, 45.0)
+        
+        CGContextSetRGBStrokeColor(context, red, green, blue, opacity)
+        CGContextStrokePath(context)
+        imageViewBrush.image = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+    }
 }
--- PicPaint/ImageEditorViewController.swift
+++ PicPaint/ImageEditorViewController.swift
@@ -8,8 +8,15 @@
 
 import UIKit
 
-class ImageEditorViewController: UIViewController {
+class ImageEditorViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     // MARK: - Properties
+    var lastPoint = CGPoint.zeroPoint
+    var red: CGFloat = 0.0
+    var green: CGFloat = 0.0
+    var blue: CGFloat = 0.0
+    var brushWidth: CGFloat = 10.0
+    var opacity: CGFloat = 1.0
+    var swiped = false
     
     @IBOutlet weak var mainImageView: UIImageView!
     @IBOutlet weak var tempDrawImageView: UIImageView!
@@ -18,7 +25,9 @@ class ImageEditorViewController: UIViewController {
     // MARK: - Actions
     
     @IBAction func didPressDelete(sender: UIBarButtonItem) {
+        mainImageView.image = nil
     }
+    
     @IBAction func didPressShare(sender: UIBarButtonItem) {
         // TODO: Merge all Image Views into one mainImageView
         UIGraphicsBeginImageContext(mainImageView.bounds.size)
@@ -27,37 +36,135 @@ class ImageEditorViewController: UIViewController {
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         
+        
         let activity = UIActivityViewController(activityItems: [image], applicationActivities: nil)
         presentViewController(activity, animated: true, completion: nil)
     }
     
     @IBAction func didPressTakePhoto(sender: UIBarButtonItem) {
+        if (UIImagePickerController.availableCaptureModesForCameraDevice(.Rear) != nil) {
+            let picker = UIImagePickerController()
+            picker.sourceType = UIImagePickerControllerSourceType.Camera
+            picker.delegate = self
+            presentViewController(picker, animated: true, completion: nil)
+        }
+        else {
+            noCamera()
+        }
     }
     
     @IBAction func didPressAlbum(sender: UIBarButtonItem) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        presentViewController(picker, animated: true, completion: nil)
     }
     
+    // MARK: - UIImagePickerControllerDelegate
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            mainImageView.image = image
+        }
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    /// alert if no rear camera available
+    func noCamera(){
+        let alertVC = UIAlertController(title: "No Camera", message: "Sorry, this device has no camera", preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "OK", style:.Default, handler: nil)
+        alertVC.addAction(okAction)
+        presentViewController(alertVC, animated: true, completion: nil)
+    }
+    
+  
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        swiped = false
+        if let touch = touches.first as? UITouch {
+            lastPoint = touch.locationInView(self.view)
+        }
+       
+    }
+    
+    func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint) {
+        
+        UIGraphicsBeginImageContext(view.frame.size)
+        let context = UIGraphicsGetCurrentContext()
+        tempDrawImageView.image?.drawInRect(CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height))
+        
+        CGContextMoveToPoint(context, fromPoint.x, fromPoint.y)
+        CGContextAddLineToPoint(context, toPoint.x, toPoint.y)
+        
+        CGContextSetLineCap(context, kCGLineCapRound)
+        CGContextSetLineWidth(context, brushWidth)
+        CGContextSetRGBStrokeColor(context, red, green, blue, 1.0)
+        CGContextSetBlendMode(context, kCGBlendModeNormal)
+        
+        CGContextStrokePath(context)
+        
+        tempDrawImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        tempDrawImageView.alpha = opacity
+        UIGraphicsEndImageContext()
+        
+    }
+    
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        swiped = true
+        if let touch = touches.first as? UITouch {
+            let currentPoint = touch.locationInView(view)
+            drawLineFrom(lastPoint, toPoint: currentPoint)
+            
+            lastPoint = currentPoint
+        }
+    }
+    
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        
+        if !swiped {
+            // draw a single point
+            drawLineFrom(lastPoint, toPoint: lastPoint)
+        }
+        
+        // Merge tempImageView into mainImageView
+        UIGraphicsBeginImageContext(mainImageView.frame.size)
+        mainImageView.image?.drawInRect(CGRect(x: 0, y: 0, width: mainImageView.frame.size.width, height: mainImageView.frame.size.height), blendMode: kCGBlendModeNormal, alpha: 1.0)
+        tempDrawImageView.image?.drawInRect(CGRect(x: 0, y: 0, width: mainImageView.frame.size.width, height: mainImageView.frame.size.height), blendMode: kCGBlendModeNormal, alpha: opacity)
+        mainImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        tempDrawImageView.image = nil
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        let settingsViewController = segue.destinationViewController as! BrushSettingsViewController
+        settingsViewController.delegate = self
+        settingsViewController.brush = brushWidth
+        settingsViewController.opacity = opacity
+        settingsViewController.red = red
+        settingsViewController.green = green
+        settingsViewController.blue = blue
     }
-    */
-
+    
 }
+
+extension ImageEditorViewController: BrushSettingsViewControllerDelegate {
+    func settingsViewControllerFinished(settingsViewController: BrushSettingsViewController) {
+        self.brushWidth = settingsViewController.brush
+        self.opacity = settingsViewController.opacity
+        self.red = settingsViewController.red
+        self.green = settingsViewController.green
+        self.blue = settingsViewController.blue
+    }
+}
+
